9a801dbee7dee8715e69f12bf6015e08f480ede3
samgozman <eragonovich@gmail.com>
2021-01-12 00:10:19 +0300

    Export valid parsed JSON data

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,29 +1,44 @@
 const got = require('got')
 const cheerio = require('cheerio')
 
-const url = 'https://www.tinkoff.ru/invest/margin/equities/'
-
+/**
+ * Parse tinkoff margin equities page and collect data in JSON format
+ * @returns {Object} Object of tickers objects
+ */
 const parse  = async () => {
     try {
-        const response = await got(url)
+        const response = await got('https://www.tinkoff.ru/invest/margin/equities/')
         
         if(!response.body) throw new Error('Parsed body is undefined')
 
         const $ = cheerio.load(response.body)
         const table = $('table').first().find('tbody').first().find('tr')
+        let jsonData = {}
 
-        var tableArray = Array.prototype.map.call(table, (tr) => {
+        /**
+         * Create array of objects by parsing the DOM table
+         * @returns {Object} Array of objects
+         * Object format: 
+         * ticker: { name, isin, short, longRisk, shortRisk }
+         */
+        let tableObjArray = [] = Array.prototype.map.call(table, (tr) => {
             return {
-                name: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(1)').text(),
-                ticker: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(2)').text().split(',')[0],
-                isin: $(tr).find('td:nth-of-type(2) a span div div:nth-of-type(1)').text(),
-                short: $(tr).find('td:nth-of-type(3) a span div:nth-of-type(1)').text() === 'Доступен' ? true : false,
-                longRisk: $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[0].trim(),
-                shortRisk: $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[1].trim()
+                [ $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(2)').text().split(',')[0] ]: {
+                    name: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(1)').text(),
+                    isin: $(tr).find('td:nth-of-type(2) a span div div:nth-of-type(1)').text(),
+                    short: $(tr).find('td:nth-of-type(3) a span div:nth-of-type(1)').text() === 'Доступен' ? true : false,
+                    longRisk: parseFloat( $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[0].trim() ),
+                    shortRisk: parseFloat( $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[1].trim() )
+                }
             }
         })
+        
+        // Convert objects array into object (for JSON)
+        tableObjArray.forEach(element => {
+            Object.assign(jsonData, element)
+        })
 
-        return tableArray
+        return jsonData
     } catch (error) {
         throw new Error(error)
     }
