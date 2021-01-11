6f34499836e7e72d66dc2071d68059375a4e2dca
samgozman <eragonovich@gmail.com>
2021-01-11 19:03:00 +0300

    Parsing data: name, ticker, isin (etc)

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -13,7 +13,14 @@ const parse  = async () => {
         const table = $('table').first().find('tbody').first().find('tr')
 
         var tableArray = Array.prototype.map.call(table, (tr) => {
-            return $(tr).find('td:nth-of-type(1) span div ').text()
+            return {
+                name: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(1)').text(),
+                ticker: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(2)').text().split(',')[0],
+                isin: $(tr).find('td:nth-of-type(2) a span div div:nth-of-type(1)').text(),
+                short: $(tr).find('td:nth-of-type(3) a span div:nth-of-type(1)').text() === 'Доступен' ? true : false,
+                longRisk: $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[0].trim(),
+                shortRisk: $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[1].trim()
+            }
         })
 
         return tableArray
