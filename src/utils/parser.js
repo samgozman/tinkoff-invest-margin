16dd9c3b10c2d3701df2f0a0b1d1201fbf6689a7
samgozman <eragonovich@gmail.com>
2021-01-12 10:30:46 +0300

    Beautify +

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -3,13 +3,15 @@ const cheerio = require('cheerio')
 
 /**
  * Parse tinkoff margin equities page and collect data in JSON format
- * @returns {Object} Object of tickers objects
+ *
+ * @async
+ * @returns {Promise<object>} Object of tickers objects
  */
-const parse  = async () => {
+const parse = async () => {
     try {
         const response = await got('https://www.tinkoff.ru/invest/margin/equities/')
-        
-        if(!response.body) throw new Error('Parsed body is undefined')
+
+        if (!response.body) throw new Error('Parsed body is undefined')
 
         const $ = cheerio.load(response.body)
         const table = $('table').first().find('tbody').first().find('tr')
@@ -17,22 +19,23 @@ const parse  = async () => {
 
         /**
          * Create array of objects by parsing the DOM table
+         * 
          * @returns {Object} Array of objects
          * Object format: 
          * ticker: { name, isin, short, longRisk, shortRisk }
          */
         let tableObjArray = [] = Array.prototype.map.call(table, (tr) => {
             return {
-                [ $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(2)').text().split(',')[0] ]: {
+                [$(tr).find('td:nth-of-type(1) a span div div:nth-of-type(2)').text().split(',')[0]]: {
                     name: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(1)').text(),
                     isin: $(tr).find('td:nth-of-type(2) a span div div:nth-of-type(1)').text(),
                     short: $(tr).find('td:nth-of-type(3) a span div:nth-of-type(1)').text() === 'Доступен' ? true : false,
-                    longRisk: parseFloat( $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[0].trim() ),
-                    shortRisk: parseFloat( $(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[1].trim() )
+                    longRisk: parseFloat($(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[0].trim()),
+                    shortRisk: parseFloat($(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[1].trim())
                 }
             }
         })
-        
+
         // Convert objects array into object (for JSON)
         tableObjArray.forEach(element => {
             Object.assign(jsonData, element)
