3622ca5eb5fe8fb0c5f0f66fa71e6d6141ba3794
samgozman <eragonovich@gmail.com>
2021-01-15 19:51:34 +0300

    Temporary removing ISIN

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -32,7 +32,7 @@ const parse = async () => {
             return {
                 ticker: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(2)').text().split(',')[0],
                 name: $(tr).find('td:nth-of-type(1) a span div div:nth-of-type(1)').text(),
-                isin: $(tr).find('td:nth-of-type(2) a span div div:nth-of-type(1)').text(),
+                // isin: $(tr).find('td:nth-of-type(2) a span div div:nth-of-type(1)').text(),
                 short: $(tr).find('td:nth-of-type(3) a span div:nth-of-type(1)').text() === 'Доступен' ? true : false,
                 longRisk: parseFloat($(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[0].trim()),
                 shortRisk: parseFloat($(tr).find('td:nth-of-type(4) a span div:nth-of-type(1)').text().split('/')[1].trim())
